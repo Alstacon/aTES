b5c97a5626ed77c2181f1bf41930256d14c301e1
alstacon <alstacon@gmail.com>
2024-03-13 16:29:40 +0300

    update users model

--- services/auth/src/models/users.py
+++ services/auth/src/models/users.py
@@ -13,7 +13,18 @@ class UserRoles(enum.Enum):
 
 
 class User(Base):
-    public_id: Mapped[str] = mapped_column(String(50), unique=True, nullable=False, default=get_public_id())
+    public_id: Mapped[str] = mapped_column(
+        String(50), unique=True, nullable=False, default=get_public_id
+    )
     username: Mapped[str] = mapped_column(String(32), unique=True, nullable=False)
     email: Mapped[str] = mapped_column(String(50), unique=True, nullable=False)
-    role: Mapped[str] = mapped_column(Enum(UserRoles), default=UserRoles.EMPLOYEE, nullable=False)
+    role: Mapped[UserRoles] = mapped_column(
+        Enum(UserRoles), default=UserRoles.EMPLOYEE, nullable=False
+    )
+    password: Mapped[str] = mapped_column(String(16), nullable=False)
+
+    def __str__(self):
+        return f"{self.__class__.__name__}(id={self.id}, username={self.username!r})"
+
+    def __repr__(self):
+        return str(self)
