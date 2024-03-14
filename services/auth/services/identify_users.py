956d5640f95233e508a4a1123b85a9077a68d0c9
alstacon <alstacon@gmail.com>
2024-03-14 16:18:38 +0300

    finiched validate_auth_user method

--- services/auth/services/identify_users.py
+++ services/auth/services/identify_users.py
@@ -1,9 +1,28 @@
 import uuid
 
-from fastapi import Depends
+from fastapi import Depends, Form
+from sqlalchemy.ext.asyncio import AsyncSession
+from starlette import status
 
+from services.exceptions import UnauthException
 from services.issue_tokens import get_current_token_payload
-from src.schemas.users import InfoUserSchema
+from services import users as users_service
+from src import utils
+from src.schemas.users import InfoUserSchema, LogInUserSchema
+
+
+async def validate_auth_user(session: AsyncSession, user_info: LogInUserSchema):
+    if not (
+        db_user := await users_service.get_user_by_username(
+            session=session, username=user_info.username
+        )
+    ):
+        raise UnauthException
+    if not utils.validate_password(
+        password=user_info.password, hashed_password=db_user.password
+    ):
+        raise UnauthException
+    return db_user
 
 
 def get_current_auth_user(
